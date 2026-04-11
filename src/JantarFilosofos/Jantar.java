31e075e4263f85db334e642dd0cf9e7257abde70
José Antonio Santos <joseantonio.santos@ucsal.edu.br>
2026-04-11 07:46:44 -0300

    "Update pags 20260411_074655"

--- src/JantarFilosofos/Jantar.java
+++ src/JantarFilosofos/Jantar.java
@@ -1,17 +1,5 @@
 package JantarFilosofos;
 
-/*
-O JANTAR DOS FILÓSOFOS - PROBLEMA DE SINCRONIZAÇÃO EM SISTEMAS OPERACIONAIS
-Um dos problemas clássicos sobre a sincronização na comunicação entre processos e threads em Sistemas Operacionais.
-
-Cinco filósofos estão sentados em uma mesa redonda para jantar. Cada filósofo tem um prato com espaguete à sua frente. 
-Cada prato possui um garfo para pegar o espaguete. O espaguete está muito escorregadio e, para que um filósofo consiga comer, 
-será necessário utilizar dois garfos. 
-
-*/
-
-import java.awt.BorderLayout;
-
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -22,16 +10,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import JantarFilosofos.filosofo;
-import JantarFilosofos.garfos;
 import java.awt.Toolkit;
-import java.awt.Color;
 
 public class Jantar extends JFrame {
 	private static final long serialVersionUID = 8531554653309568273L;
 	private Image fundo,arist,descar,pitagor,socrat,plat,mes;
-	private filosofo f0, f1, f2, f3, f4;
-	public garfos fork;	
+	private Filosofo f0, f1, f2, f3, f4;
+	public Garfos fork;	
 	private JPanel contentPane;
 
 	public static void main(String[] args) {
@@ -70,18 +55,20 @@ public class Jantar extends JFrame {
 		if (chave == 0) {
 			
 			if (estado == 0) {
-				ImageIcon referencia = new ImageIcon("src\\Imagens\\aristoteles.png");		
+				ImageIcon referencia = new ImageIcon("src\\Imagens\\arlequina00.png");		
 				arist = referencia.getImage();
 				
 			} else if (estado == 1) {
-				ImageIcon referencia = new ImageIcon("src\\Imagens\\aristoteles2.png");		
+				ImageIcon referencia = new ImageIcon("src\\Imagens\\arlequina11.png");		
 				arist = referencia.getImage();
 				
 			} else if (estado == 2) {
-				ImageIcon referencia = new ImageIcon("src\\Imagens\\aristoteles1.png");		
+				ImageIcon referencia = new ImageIcon("src\\Imagens\\arlequina22.png");		
 				arist = referencia.getImage();				
 			}
+			
 		} else if (chave == 1) {
+			
 			if (estado == 0) {
 				ImageIcon referencia = new ImageIcon("src\\Imagens\\platao.png");		
 				plat = referencia.getImage();
@@ -96,6 +83,7 @@ public class Jantar extends JFrame {
 				plat = referencia.getImage();
 			
 			}
+			
 		} else if (chave == 2) {
 			
 			if (estado == 0) {
@@ -110,7 +98,9 @@ public class Jantar extends JFrame {
 				ImageIcon referencia = new ImageIcon("src\\Imagens\\descartes1.png");		
 				descar = referencia.getImage();
 			}
+			
 		} else if (chave == 3) {
+			
 			if (estado == 0) {
 				ImageIcon referencia = new ImageIcon("src\\Imagens\\socrates.png");		
 				socrat = referencia.getImage();
@@ -143,17 +133,17 @@ public class Jantar extends JFrame {
 		}
 		
 		repaint();
-	}	
+	}
 	
 	/**
 	 * Create the frame.
 	 */
 	
 	public Jantar() {
-		setTitle("Jantar dos Fil\u00F3sofos -> Trabalho de Sistemas Operacionais");
+		setTitle("Jantar dos Filosofos -> Trabalho de Sistemas Operacionais");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Jantar.class.getResource("/Imagens/Icon.png")));
 			
-		ImageIcon referencia = new ImageIcon("src\\Imagens\\fundo.png");		
+		ImageIcon referencia = new ImageIcon("src\\Imagens\\fundo1.png");		
 		fundo = referencia.getImage();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 515, 597);
@@ -165,12 +155,12 @@ public class Jantar extends JFrame {
 		
 		referencia = new ImageIcon("src\\Imagens\\mesa.png");		
 		
-		fork = new garfos(); 
-		f0 = new filosofo(0, this); 
-		f1 = new filosofo(1, this);
-		f2 = new filosofo(2, this);
-		f3 = new filosofo(3, this);
-		f4 = new filosofo(4, this);
+		fork = new Garfos(); 
+		f0 = new Filosofo(0, this); 
+		f1 = new Filosofo(1, this);
+		f2 = new Filosofo(2, this);
+		f3 = new Filosofo(3, this);
+		f4 = new Filosofo(4, this);
 		
 		f0.start();
 		f1.start();
@@ -178,8 +168,6 @@ public class Jantar extends JFrame {
 		f3.start();
 		f4.start();
 		
-		
-			
 	}
 	
 	public void start() {
